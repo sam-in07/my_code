d5f5ff8a0d4fb920c9df7649061e887a7a1943a4
sam-in07 <85004796+sam-in07@users.noreply.github.com>
2024-07-03 13:38:24 +0600

    Create dfs.cpp

---
+++ dfs.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+vector<ll> v[INT16_MAX];
+// for creating adj list 
+ll vis[INT16_MAX];
+// visited array
+
+void dfs(ll node) {
+  vis[node] = 1;// visted cuurent  node 
+  for(ll child : v[node]) 
+  // asj od this node 
+  {
+    if(!vis[child])
+    // checking those node are visisted or not 
+    dfs(child); 
+    // if not visited creat dfs 
+  }
+}
+
+int main () {
+  ll i , n , e , a , b ;
+  cin >> n >> e ;
+  for(i=1;i<=e;i++) {
+    cin >> a >> b ;
+    v[a].push_back(b);
+    //adj list of a we have to insert b 
+    v[b].push_back(a);
+     //adj list of b we have to insert a
+  }
+  
+ 
+  ll cc = 0 ;
+  for(i=1;i<=n;i++) 
+  if(!vis[i]) // not visited
+ dfs(i) , cc++;
+  
+  cout << cc << endl;
+
+}
